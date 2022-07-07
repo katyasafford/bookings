08a3c05f0656833a5b5723fe32d8f878935c7a62
Katya Safford <katerin2011@gmail.com>
2022-07-07 16:16:43 -0500

    initial commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/katyasafford/bookings/pkg/config"
+	"github.com/katyasafford/bookings/pkg/models"
+	"github.com/katyasafford/bookings/pkg/render"
+)
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+var Repo *Repository
+
+// creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "hello again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	// render data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
