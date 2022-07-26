f00e306ee190da2d92d0b14cf1014df9fec7ee96
Katya Safford <katerin2011@gmail.com>
2022-07-26 14:58:24 -0600

    add server-side validation to reservation

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,12 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
 )
 
 // creates a custom form struct, embeds url.Values object
@@ -23,7 +27,7 @@ func New(data url.Values) *Form {
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
-		f.Errors.Add(field, "this field cannot be blank")
+		// f.Errors.Add(field, "this field cannot be blank")
 		return false
 	}
 	return true
@@ -32,4 +36,30 @@ func (f *Form) Has(field string, r *http.Request) bool {
 // returns true if there are no errors in form, otherwise returns false
 func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
+}
+
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field can not be blank")
+		}
+	}
+}
+
+// checks for string minimum length
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
+		return false
+	}
+	return true
+}
+
+//check for valid email address
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address")
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -86,7 +86,9 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	form := forms.New(r.PostForm)
 
-	form.Has("first_name", r)
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, r)
+	form.IsEmail("email")
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
