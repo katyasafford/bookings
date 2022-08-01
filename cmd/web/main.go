e619b05de0455660772fc3642ee2f49036ddd07a
Katya Safford <katerin2011@gmail.com>
2022-08-01 11:14:44 -0600

    create helper functions for error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/katyasafford/bookings/internal/config"
 	"github.com/katyasafford/bookings/internal/handlers"
+	"github.com/katyasafford/bookings/internal/helpers"
 	"github.com/katyasafford/bookings/internal/models"
 	"github.com/katyasafford/bookings/internal/render"
 
@@ -19,6 +21,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 	err := run()
@@ -46,6 +50,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	// set up the session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
@@ -66,7 +76,8 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
+
 	return nil
 }
