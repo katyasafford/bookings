2b645497cf7552afeed61fe11515ea3df59b0fc1
Katya Safford <katerin2011@gmail.com>
2022-08-31 09:37:36 -0500

    handlers for searching availability

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,16 @@
 package repository
 
-import "github.com/katyasafford/bookings/internal/models"
+import (
+	"time"
+
+	"github.com/katyasafford/bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
+	SearchAvailalbilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
