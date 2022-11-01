918cfc2290de9f9e07d4d0fe62310d646bb18464
Katya Safford <katerin2011@gmail.com>
2022-11-01 09:37:40 -0500

    logout handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -435,3 +435,14 @@ func (m *Repository) PostShowLogin(w http.ResponseWriter, r *http.Request) {
 	m.App.Session.Put(r.Context(), "flash", "Successfully Logged In")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
+	_ = m.App.Session.Destroy(r.Context())
+	_ = m.App.Session.RenewToken(r.Context())
+
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+}
+
+func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-dashboard.page.tmpl", &models.TemplateData{})
+}
